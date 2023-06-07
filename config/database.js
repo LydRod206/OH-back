19b8fb6528b6640f677d6dcd4af5c45b214464e3
stasiaDiamond <nsisco@uw.edu>
2023-06-07 09:58:05 -0700

    JAWS7

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
 // const sequelize = require('./config/sequelize');
-// Adjust the path based on your project structure
 
 require('dotenv').config();
 console.log(process.env.JAWSDB_URL);
@@ -8,18 +7,18 @@ console.log(process.env.JAWSDB_URL);
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+    } else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+        }
+    );
 }
 
 module.exports = sequelize;
