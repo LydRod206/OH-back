cec3376037c51d3b8ae3fa2d401ac333c9c9dfd0
stasiaDiamond <nsisco@uw.edu>
2023-06-08 14:28:58 -0700

    updating

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
+
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
 
 const controllers = require('./controllers');
-
-
-// Import routes
-// const userRoutes = require('./routes/userRoutes');
-// const allRoutes = require('./controllers/');
-// const mainRoutes = require('./controllers/mainRoutes');
 
 
 // var testAPIRouter = require("./routes/testAPI");
@@ -17,12 +12,13 @@ const controllers = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+// Set up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Enable CORS for specific origin
 app.use(cors({
-  origin: ['https://only-hands.netlify.app', 'http://localhost:3000'],
+  origin: ['https://only-hands.netlify.app','http://localhost:3000'],
   optionsSuccessStatus: 200
 }));
 
@@ -32,7 +28,6 @@ app.use('/', controllers);
 // app.use('/', allRoutes);
 // app.use("/", mainRoutes);
 
-app.use('/', controllers);
 
 // app.use("/testAPI", testAPIRouter);
 
@@ -44,60 +39,6 @@ sequelize.sync({ force: false }).then(() => {
 });
 
 module.exports = app;
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const sequelize = require('./config/database');
-
-// const controllers = require('./controllers');
-
-
-// // Import routes
-// // const userRoutes = require('./routes/userRoutes');
-// // const allRoutes = require('./controllers/');
-// // const mainRoutes = require('./controllers/mainRoutes');
-
-
-// // var testAPIRouter = require("./routes/testAPI");
-
-// // Create Express app
-// const app = express();
-// const PORT = process.env.PORT || 9000;
-
-// // Set up middleware
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // Enable CORS for specific origin
-// app.use(cors({
-//   origin: ['https://only-hands.netlify.app','http://localhost:3000'],
-//   optionsSuccessStatus: 200
-// }));
-
-// // Set up routes
-// app.use('/', controllers);
-// // app.use('/users', userRoutes);
-// // app.use('/', allRoutes);
-// // app.use("/", mainRoutes);
-
-
-// // app.use("/testAPI", testAPIRouter);
-
-// // Sync database and start server
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => {
-//     console.log('Backend Server running on PORT ' + PORT);
-//   });
-// });
-
-// module.exports = app;
 
 
 
