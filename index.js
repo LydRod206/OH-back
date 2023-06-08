41fd99c25981eb72fec84dd7cd2c1cf78329866c
stasiaDiamond <nsisco@uw.edu>
2023-06-08 14:10:59 -0700

    updating

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const cors = require('cors');
 const sequelize = require('./config/database');
 
 const controllers = require('./controllers');
+
+
+// Import routes
+// const userRoutes = require('./routes/userRoutes');
+// const allRoutes = require('./controllers/');
+// const mainRoutes = require('./controllers/mainRoutes');
+
+
+// var testAPIRouter = require("./routes/testAPI");
+
+// Create Express app
 const app = express();
 const PORT = process.env.PORT || 9000;
 
@@ -18,6 +29,15 @@ app.use(cors({
 // Set up routes
 app.use('/api', controllers);
 
+app.use('/', controllers);
+// app.use('/users', userRoutes);
+// app.use('/', allRoutes);
+// app.use("/", mainRoutes);
+
+
+// app.use("/testAPI", testAPIRouter);
+
+// Sync database and start server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log('Backend Server running on PORT ' + PORT);
